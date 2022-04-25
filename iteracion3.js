6ae26f18cdae0bd658b9ba66ca3abf12180d8653
alvaroFGG <alvaro@mbp-de-alvaro.home>
2022-04-25 17:28:50 +0200

    subida de las 3 primeras iteraciones

---
+++ iteracion3.js
@@ -0,0 +1,41 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+
+
+// 1.1
+alert(10*5);
+
+
+// 1.2
+alert(10/2);
+
+
+// 1.3
+alert(15%9);
+
+
+// 1.4
+let x = 0;
+let y = 10;
+let z = 5;
+
+x = y+z;
+alert(x)
+
+
+// 1.5
+let x = 0;
+let y = 10;
+let z = 5;
+
+x = y*z;
+alert(x)
